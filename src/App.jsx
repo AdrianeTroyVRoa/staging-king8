9fdffa5c7829a1e668f81ff09ae2f23b7eb79800
Adriane Troy V. Roa <20210023165@my.xu.edu.ph>
2024-04-23 00:54:27 +0800

    added initial header menu contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import styles from "./App.module.css";
+import Header from "./Header.jsx";
 
 function App() {
   return (
-    <h1 class="text-3xl font-bold text-green-500">
-      Hello world!
-    </h1>
+    <>
+      <Header />
+      <h1 class="text-3xl font-bold text-green-500">Hello world!</h1>
+    </>
   );
 }
 
