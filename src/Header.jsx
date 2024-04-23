42f5266839367cbdabb2d6e7d753d27c70615927
Adriane Troy V. Roa <20210023165@my.xu.edu.ph>
2024-04-23 11:46:46 +0800

    Changed font size for header

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
 
   return (
     <nav className="bg-dark-100 px-4 lg:px-6 py-2.5">
-      <div className="flex flex-wrap text-blue-950 justify-between items-center mx-auto max-w-screen-xl">
+      <div className="flex flex-wrap text-blue-950 justify-between items-center mx-auto max-w-screen-xl text-sm">
         <a href="#" className="flex items-center">
           <img
             src={king8}
@@ -14,7 +14,7 @@ function Header() {
           />
         </a>
         <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
-          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+          <ul className="flex flex-col mt-4 lg:flex-row lg:space-x-8 lg:mt-0">
             {menuItems.map((item, index) => (
               <li key={index}>
                 <a className="block py-2 pr-4 pl-3 font-bold">
@@ -25,7 +25,7 @@ function Header() {
           </ul>
         </div>
         <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
-          <div className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+          <div className="flex flex-col mt-4 lg:flex-row lg:space-x-8 lg:mt-0">
             <a className="font-bold bg-amber-400 text-light py-3 px-4 rounded-lg">SIGN UP</a>
             <a className="font-bold border-solid border-2 border-blue-950 text-light py-3 px-4 rounded-lg">LOG IN</a>
           </div>
