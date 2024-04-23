b66f1ba2f8172824ee6ddd146dc470f026bc8166
Adriane Troy V. Roa <20210023165@my.xu.edu.ph>
2024-04-23 16:19:46 +0800

    Changed header colors to match figma design and added favicon

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   const menuItems = ["HOME", "PRODUCTS", "ABOUT US", "CONTACT US"];
 
   return (
-    <nav className="bg-dark-100 px-4 lg:px-6 py-2.5">
+    <nav className="bg-zinc-100 px-4 lg:px-6 py-2.5">
       <div className="flex flex-wrap text-blue-950 justify-between items-center mx-auto max-w-screen-xl text-sm">
         <a href="#" className="flex items-center">
           <img
@@ -17,7 +17,7 @@ function Header() {
           <ul className="flex flex-col mt-4 lg:flex-row lg:space-x-8 lg:mt-0">
             {menuItems.map((item, index) => (
               <li key={index}>
-                <a className="block py-2 pr-4 pl-3 font-bold">
+                <a className="block py-2 pr-4 pl-3 font-bold hover:text-blue-700">
                   {item}
                 </a>
               </li>
@@ -26,8 +26,8 @@ function Header() {
         </div>
         <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
           <div className="flex flex-col mt-4 lg:flex-row lg:space-x-8 lg:mt-0">
-            <a className="font-bold bg-amber-400 text-light py-3 px-4 rounded-lg">SIGN UP</a>
-            <a className="font-bold border-solid border-2 border-blue-950 text-light py-3 px-4 rounded-lg">LOG IN</a>
+            <a className="font-bold bg-amber-400 text-light py-3 px-4 rounded-lg hover:bg-amber-500">SIGN UP</a>
+            <a className="font-bold border-solid border-2 border-blue-950 text-light py-3 px-4 rounded-lg hover:bg-white">LOG IN</a>
           </div>
         </div>
       </div>
