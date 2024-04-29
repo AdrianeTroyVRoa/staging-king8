a57ed4958f4683db1a21f78b9e90ed5df95f2e0e
Adriane Troy V. Roa <20210023165@my.xu.edu.ph>
2024-04-29 21:16:19 +0800

    Adjust flex config in Visit Us section

--- src/components/Visit.jsx
+++ src/components/Visit.jsx
@@ -1,8 +1,8 @@
 function Visit() {
   return (
     <section className="bg-zinc-100 bg-cover bg-center flex items-center justify-center">
-      <div className="lg:flex-row sm:flex-col sm:items-center my-20 mx-10">
-        <div className="flex-none lg:w-1/2 sm:h-1/2 mr-4">
+      <div className="flex flex-wrap container my-20 mx-10">
+        <div className="flex lg:w-1/2 sm:h-1/2 mr-5 item-center">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1973.1489350543252!2d124.70756480270668!3d8.470388325831761!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32ff8d8088225c21%3A0x69eb7486adf52c59!2sMindanao%20King8%20Plastics!5e0!3m2!1sen!2sph!4v1714371034718!5m2!1sen!2sph"
             width="600"
@@ -13,9 +13,9 @@ function Visit() {
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
-        <div className="flex flex-col lg:w-1/2 sm:h-1/2 text-blue-950">
+        <div className="flex flex-col lg:w-1/2 sm:h-1/2 text-blue-950 ml-5 container">
           <h1 className="font-bold text-5xl">VISIT OUR OFFICE</h1>
-          <p className="text-lg">
+          <p className="text-lg text-justify">
             Discover genuine quality plastics crafted right here in the heart of
             Cagayan de Oro, Philippines. Nestled within the bustling district of
             Baloy, our manufacturing hub at Bldg 104 Kimwa is more than just an
