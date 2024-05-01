a4f468ece0ce2a2634a564c079ea643124156fac
Mel Angelo Mendez <200831531@my.xu.edu.ph>
2024-05-01 16:08:46 +0800

    revised about us page

--- src/pages/about/AboutUs.jsx
+++ src/pages/about/AboutUs.jsx
@@ -1,39 +1,42 @@
 import Footer from "./Footer";
-import Header from "./Header";
 import header from "./assets/Header_Sample.png";
+import Header from "./components/Header";
 
 function About(){
     return(
-        <div className="relative bg-zinc-100 w-screen h-[2500px]">
+        <div className="relative bg-zinc-100 w-screen h-screen">
             <Header/>
             <div className="w-screen h-72 relative bg-stone-950">
                 <div className="w-screen h-24 font-extrabold mt-24 flex justify-center items-center text-zinc-100 text-5xl absolute z-10 ">ABOUT US</div>
                     <img className="w-screen h-72 opacity-35" src={header}/>
-                    <div className="w-screen h-160 relative bg-stone-950 top-0">
-                    
-                </div>
-
             </div>
 
-            <div className="w-screen h-160 relative bg-blue-950 top-0">
-                <div className="container w-96 h-72 absolute bg-zinc-300 flex items-center justify-between top-40 left-1/2 rounded-xl mx-36">
-                    <div className="w-48 h-8 left-8 top-16 font-bold mt-0 flex items-center text-blue-950 text-2xl absolute z-10">Proudly Local</div>
-                    <div className="w-48 h-24 left-8 top-16 font-normal mt-8 flex items-center text-blue-950 text-lg absolute z-10">Mindanao King8 Plastic is based in Cagayan de Oro City, Philippines.</div>
-                </div>
-            </div>
-
-            <div className="w-screen h-160 relative bg-stone-950 top-160">
-                <img className="w-screen h-full opacity-35" src={header}/>
-                <div className="container w-96 h-72 absolute bg-blue-950 flex items-center justify-between top-40 left-1/2 rounded-xl mx-36">
-                    <div className="w-48 h-0 left-8 top-16 font-bold mt-0 flex items-center text-zinc-100 text-2xl absolute z-10">Manufacturing</div>
-                    <div className="w-60 h-32 left-8 top-24 font-normal mt-0 flex items-center text-zinc-100 text-lg absolute z-10">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+            <div className="w-screen h-160 relative bg-blue-950">
+                <div className="2xl:container 2xl:mx-auto lg:py-48 lg:px-36 md:py-24 md:px-12 py-24 px-12">
+                    <div className="flex flex-col lg:flex-row gap-8">
+                        <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                            <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-zinc-100 pb-4">Proudly Local</h1>
+                            <p className="font-normal text-base leading-6 text-zinc-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam id diam maecenas ultricies. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Eu volutpat odio facilisis mauris sit. Tincidunt vitae semper quis lectus nulla at volutpat diam ut. Fringilla est ullamcorper eget nulla facilisi etiam dignissim. </p>
+                        </div>
+                        <div className="w-full lg:w-8/12">
+                            <img className="w-full h-full rounded-lg" src={header}></img>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            <div className="w-screen h-160 relative bg-zinc-100 top-160">
-                <div className="container w-96 h-72 absolute bg-zinc-100 flex items-center justify-between top-40 left-1/2 rounded-xl mx-36 stroke-blue-950 stroke-2">
-                    <div className="w-48 h-0 left-8 top-16 font-bold mt-0 flex items-center text-blue-950 text-2xl absolute z-10">Quality Products For You</div>
-                    <div className="w-60 h-32 left-8 top-24 font-normal mt-0 flex items-center text-blue-950 text-lg absolute z-10">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+            <div className="w-screen relative bg-zinc-100">
+                <div className="2xl:container 2xl:mx-auto lg:py-60 lg:px-36 md:py-36 md:px-12 sm:py-36 py-24 px-12">
+                    <div className="flex flex-col lg:flex-row justify-between gap-16">
+                        <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                            <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-blue-950 pb-4">Quality Products for You</h1>
+                            <p className="font-normal text-base leading-6 text-blue-950">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam id diam maecenas ultricies. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Eu volutpat odio facilisis mauris sit. Tincidunt vitae semper quis lectus nulla at volutpat diam ut. Fringilla est ullamcorper eget nulla facilisi etiam dignissim. </p>
+                        </div>
+                        <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                            <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-blue-950 pb-4">Manufacturing</h1>
+                            <p className="font-normal text-base leading-6 text-blue-950">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam id diam maecenas ultricies. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Eu volutpat odio facilisis mauris sit. Tincidunt vitae semper quis lectus nulla at volutpat diam ut. Fringilla est ullamcorper eget nulla facilisi etiam dignissim. </p>                            
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer/>
