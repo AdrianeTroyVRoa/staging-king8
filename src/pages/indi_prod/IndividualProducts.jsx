249257ca8a78158d7cd55fad575d4b33e64c7a1b
Adriane Troy V. Roa <20210023165@my.xu.edu.ph>
2024-05-01 18:22:26 +0800

    Put image in individual products page to preview

--- src/pages/indi_prod/IndividualProducts.jsx
+++ src/pages/indi_prod/IndividualProducts.jsx
@@ -1,56 +1,65 @@
 import Header from "../../components/Header";
-
+import pipes from "../../assets/pipes.jpg"
 
 export default function IndividualProducts() {
   return (
-    <>
-    
     <div className="relative bg-zinc-100 w-full h-[1800px]">
       <Header />
       <div class="container flex mx-auto px-4 pt-20 p-20 w-1/4">
-          <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p b-20">
-                <img class="pl-6 w-full md:w-3/4 h-auto rounded-lg pr-4" src={pipes} alt="image description"/>
-                  <div>
-                    <h1 class="text-5xl md:text-5xl lg:text-6xl font-bold leading-none tracking-tight text-red-geg dark:text-blue-950 mb-4 pl-5">Pipes</h1>
-                    <p class="text-blue-950 dark:text-blue-950 text-justify pl-5">
-                    Durable, lightweight, and versatile solutions for plumbing, drainage, and industrial systems.
-                    They offer corrosion resistance, easy installation, and long-lasting performance, making them ideal for various applications.
-                    </p>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p b-20">
+          <img
+            class="pl-6 w-full md:w-3/4 h-auto rounded-lg pr-4"
+            src={pipes}
+            alt="image description"
+          />
+          <div>
+            <h1 class="text-5xl md:text-5xl lg:text-6xl font-bold leading-none tracking-tight text-red-geg dark:text-blue-950 mb-4 pl-5">
+              Pipes
+            </h1>
+            <p class="text-blue-950 dark:text-blue-950 text-justify pl-5">
+              Durable, lightweight, and versatile solutions for plumbing,
+              drainage, and industrial systems. They offer corrosion resistance,
+              easy installation, and long-lasting performance, making them ideal
+              for various applications.
+            </p>
 
-                  <div class="grid grid-cols-1 md:grid-cols-2">
-                  <form class="max-w-sm mx-0 mb-4 pt-10">
-                  <label for="countries" class="text-3xl font-bold leading-none tracking-tight text-red-geg dark:text-blue-950 mb-4 pl-5 pt-10">Quantity: </label>
+            <div class="grid grid-cols-1 md:grid-cols-2">
+              <form class="max-w-sm mx-0 mb-4 pt-10">
+                <label
+                  for="countries"
+                  class="text-3xl font-bold leading-none tracking-tight text-red-geg dark:text-blue-950 mb-4 pl-5 pt-10"
+                >
+                  Quantity:{" "}
+                </label>
 
-                     <select id="Quantity" class="font-bold border-solid border-2 border-blue-950 text-blue-950 py-3 px-4 rounded-lg hover:bg-white ">
-                        <option selected></option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                     </select>
-                   </form>
+                <select
+                  id="Quantity"
+                  class="font-bold border-solid border-2 border-blue-950 text-blue-950 py-3 px-4 rounded-lg hover:bg-white "
+                >
+                  <option selected></option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </form>
+            </div>
 
-                   
-                  </div>
-
-                    <div className="flex mt-1">
-                      <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 pt-5">
-                       <div className="flex flex-col mt-4 lg:flex-row lg:space-x-8 lg:mt-0 pl-6">
-                       <a className="font-bold bg-amber-400 text-blue-950 py-3 px-4 rounded-lg hover:bg-amber-500">INQUIRE NOW</a>
-                       <a className="font-bold border-solid border-2 border-blue-950 text-blue-950 py-3 px-4 rounded-lg hover:bg-white">ADD TO INQUIRY</a>
-                     </div>
-                     </div>
-                   </div>
-            
-                  </div>
-          
+            <div className="flex mt-1">
+              <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 pt-5">
+                <div className="flex flex-col mt-4 lg:flex-row lg:space-x-8 lg:mt-0 pl-6">
+                  <a className="font-bold bg-amber-400 text-blue-950 py-3 px-4 rounded-lg hover:bg-amber-500">
+                    INQUIRE NOW
+                  </a>
+                  <a className="font-bold border-solid border-2 border-blue-950 text-blue-950 py-3 px-4 rounded-lg hover:bg-white">
+                    ADD TO INQUIRY
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          </div>
+        </div>
+      </div>
     </div>
-
-
-
-
-    </>
   );
 }
