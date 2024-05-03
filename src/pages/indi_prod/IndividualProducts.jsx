3e62ff8ff71b1382d43ddcec9d6b5fc5fe31a6f1
Francis-Aguila <200930062@my.xu.edu.ph>
2024-05-03 16:56:07 +0800

    Fixed allignment

--- src/pages/indi_prod/IndividualProducts.jsx
+++ src/pages/indi_prod/IndividualProducts.jsx
@@ -5,18 +5,18 @@ export default function IndividualProducts() {
   return (
     <div className="relative bg-zinc-100 w-full h-[1800px]">
       <Header />
-      <div class="container flex mx-auto px-4 pt-20 p-20 w-1/4">
+      <div class="container flex mx-20 px-4 my-20 p-20 w-2">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p b-20">
           <img
-            class="pl-6 w-full md:w-3/4 h-auto rounded-lg pr-4"
+            class="pl-4 w-full md:w-3 h-auto rounded-lg pr-4"
             src={pipes}
             alt="image description"
           />
           <div>
-            <h1 class="text-5xl md:text-5xl lg:text-6xl font-bold leading-none tracking-tight text-red-geg dark:text-blue-950 mb-4 pl-5">
+            <h1 class="text-5xl md:text-5xl lg:text-6xl font-bold leading-none tracking-tight text-red-geg dark:text-blue-950 mb-4 pl-2">
               Pipes
             </h1>
-            <p class="text-blue-950 dark:text-blue-950 text-justify pl-5">
+            <p class="text-blue-950 dark:text-blue-950 text-justify pl-2">
               Durable, lightweight, and versatile solutions for plumbing,
               drainage, and industrial systems. They offer corrosion resistance,
               easy installation, and long-lasting performance, making them ideal
@@ -27,7 +27,7 @@ export default function IndividualProducts() {
               <form class="max-w-sm mx-0 mb-4 pt-10">
                 <label
                   for="countries"
-                  class="text-3xl font-bold leading-none tracking-tight text-red-geg dark:text-blue-950 mb-4 pl-5 pt-10"
+                  class="text-3xl font-bold leading-none tracking-tight text-red-geg dark:text-blue-950 mb-4 pl-2 pt-10"
                 >
                   Quantity:{" "}
                 </label>
@@ -46,8 +46,8 @@ export default function IndividualProducts() {
             </div>
 
             <div className="flex mt-1">
-              <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 pt-5">
-                <div className="flex flex-col mt-4 lg:flex-row lg:space-x-8 lg:mt-0 pl-6">
+              <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 pt-2">
+                <div className="flex flex-col mt-4 lg:flex-row lg:space-x-8 lg:mt-0 pl-2">
                   <a className="font-bold bg-amber-400 text-blue-950 py-3 px-4 rounded-lg hover:bg-amber-500">
                     INQUIRE NOW
                   </a>
