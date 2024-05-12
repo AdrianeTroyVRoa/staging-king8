0bcc7b8ecc4c13d298abf2c25fe57d57ea5485d0
Adriane Troy V. Roa <20210023165@my.xu.edu.ph>
2024-05-12 14:11:07 +0800

    Added queries for the remaining tables and configured handler for inquiry table

--- prisma/queries/wishlistQueries.js
+++ prisma/queries/wishlistQueries.js
@@ -1,1 +1,22 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
+async function createWishlist(wishlist) {
+  return prisma.wishlist.create({
+    data: wishlist,
+  });
+}
+
+async function getWishlistById(wishlistId) {
+  return prisma.wishlist.findUnique({
+    where: {
+      id: wishlistId,
+    },
+  });
+}
+
+async function getAllWishlists() {
+  return prisma.wishlist.findMany();
+}
+
+module.exports = { createWishlist, getWishlistById, getAllWishlists };
