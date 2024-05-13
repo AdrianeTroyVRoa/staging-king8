d4f09c4fb47ed27f4c7ea1e7ddff8dfbdf805d1c
Mel Angelo Mendez <200831531@my.xu.edu.ph>
2024-05-13 22:44:02 +0800

    made an upload button for product picture

--- src/pages/adminproducts/AdminProducts.jsx
+++ src/pages/adminproducts/AdminProducts.jsx
@@ -170,6 +170,11 @@ export default function AdminProducts (){
                                                                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                                                         <span class="sr-only">Close modal</span>
                                                                     </button>
+                                                                    <div className="sm:col-span-2 -mt-7">
+                                                                        <label class="text-blue-950 inline-flex border border-gray-300 justify-center items-center bg-gray-200 hover:bg-amber-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-b-lg text-sm w-48 py-2.5 text-center" for="user_avatar">Upload picture
+                                                                        </label>
+                                                                        <input class="hidden" id="user_avatar" type="file"/>
+                                                                    </div>
                                                                     <div>
                                                                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">First Name</label>
                                                                         <input type="text" name="firstname" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Default first name" required=""/>
