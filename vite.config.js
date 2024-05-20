9e390071b2ed7e3cfc001b411c6dcbbfa4d7374c
Adriane Troy V. Roa <20210023165@my.xu.edu.ph>
2024-05-20 18:11:52 +0800

    Configured vite config to keep the new config and abandon old one

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-=======
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import htmlInject from "vite-plugin-html-inject";
-import solidPlugin from "vite-plugin-solid";
-// import devtools from 'solid-devtools/vite';
-
-export default defineConfig({
-  root: "src/pages/home",
->>>>>>> development
   plugins: [
     /* 
     Uncomment the following line to enable solid-devtools.
@@ -21,50 +10,7 @@ export default defineConfig({
     */
     // devtools(),
     solidPlugin(),
-<<<<<<< HEAD
   ],
-=======
-    //pages(),
-    htmlInject({
-      files: [
-        {
-          injectTo: "body",
-          entry: "src/pages/home/index.jsx",
-          template: "src/pages/home/index.html",
-          filename: "index.html",
-        },
-        {
-          injectTo: "body",
-          entry: "src/pages/login/index.jsx",
-          template: "src/pages/login/index.html",
-          filename: "login.html",
-        },
-        {
-          injectTo: "body",
-          entry: "src/pages/register/index.jsx",
-          template: "src/pages/register/index.html",
-          filename: "register.html",
-        },
-        {
-          injectTo: "body",
-          entry: "src/pages/about/index.jsx",
-          template: "src/pages/about/index.html",
-          filename: "about.html",
-        },
-      ],
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        home: resolve(__dirname, "src/pages/home/index.html"),
-        login: resolve(__dirname, "src/pages/login/index.html"),
-        register: resolve(__dirname, "src/pages/register/index.html"),
-        about: resolve(__dirname, "src/pages/about/index.html"),
-      },
-    },
-  },
->>>>>>> development
   server: {
     port: 3000,
   },
