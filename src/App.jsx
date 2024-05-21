925dd8b31b04c18ec9b2f531650c36d4b57199c3
Adriane Troy V. Roa <20210023165@my.xu.edu.ph>
2024-05-21 08:08:01 +0800

    Added a 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import AdminProducts from "./pages/AdminProducts";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Products from "./pages/Products";
 import Register from "./pages/Register";
 import { Route } from "@solidjs/router";
 
@@ -7,8 +10,11 @@ function App() {
   return (
     <>
       <Route path="/" component={Home} />
+      <Route path="/home" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/sign-up" component={Register} />
+      <Route path="/products" component={Products} />
+      <Route path="*" component={NotFound} />
     </>
   );
 }
